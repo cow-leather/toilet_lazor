85bdb47b9264cf4b10a371ee13ca2326b35430c2
kouphasi <phasi.ok.phasi@gmail.com>
2022-09-16 01:37:22 +0900

    add made by

--- frontend/src/components/BekkuFooter.vue
+++ frontend/src/components/BekkuFooter.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="Footer">牛の革</div>
+  <div class="Footer">made by: 牛の革</div>
 </template>
 
 <style>
 .Footer {
-position: fixed; /* フッターを固定する */
-bottom: 0; /* 上部から配置の基準位置を決める */
-left: 0; /* 左から配置の基準位置を決める */
-width: 100%; /* フッターの横幅を指定する */
-height: 35px; /* フッターの高さを指定する */
-padding:10px; /* フッター内側の余白を指定する(上下左右) */
-background-color: #31a9ee; /* フッターの背景色を指定する */
-color: #FFFFFF; /* フッターのフォントの色を指定する */
+  position: fixed;
+  /* フッターを固定する */
+  bottom: 0;
+  /* 上部から配置の基準位置を決める */
+  left: 0;
+  /* 左から配置の基準位置を決める */
+  width: 100%;
+  /* フッターの横幅を指定する */
+  height: 35px;
+  /* フッターの高さを指定する */
+  padding: 10px;
+  /* フッター内側の余白を指定する(上下左右) */
+  background-color: #31a9ee;
+  /* フッターの背景色を指定する */
+  color: #FFFFFF;
+  /* フッターのフォントの色を指定する */
 }
 </style>
