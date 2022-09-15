13e2aec3a144578fb5ba67700f03113fe7b83121
phasi <koumiyom4@gmail,com>
2022-09-15 10:57:05 +0900

    信号方式を適当に作ってみた

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 // import HomeView from '../views/HomeView.vue'
-import ShowNum from "../views/ShowNum.vue"
+// import ShowNum from "../views/ShowNum.vue"
+import ShowSignal from "../views/ShowSignal.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'shownum',
-    component: ShowNum
+    path: "/",
+    name: "shownum",
+    component: ShowSignal,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
