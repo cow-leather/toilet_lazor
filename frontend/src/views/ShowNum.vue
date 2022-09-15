b7e69e8dc124555171e015a2ce7768a32b1f2234
kouphasi <phasi.ok.phasi@gmail.com>
2022-09-15 10:00:20 +0900

    数字表示ページno

---
+++ frontend/src/views/ShowNum.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1 id="number" class="number">{{ n }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            n: 0
+        }
+    }
+}
+</script>
+<style>
+.number {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
