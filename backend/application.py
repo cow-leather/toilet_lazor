33706e5a23d800c6c1e357c63a9a13e084bc8e1a
kouphasi <phasi.ok.phasi@gmail.com>
2022-09-15 22:09:51 +0900

    手を挙げている人数を取得する昨日の完成

--- backend/application.py
+++ backend/application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_restful import Api, Resource
 from count_only import request_number, files
 # from flask_cors import CORS
+from raise_hand_count_only import raise_count, files1
 
 application = Flask(__name__, static_folder='./dist/static',
                     template_folder='./dist')
@@ -13,7 +14,14 @@ class Req(Resource):
         num = request_number(files)
         return { "num" :str(num)}
 
+class RaiseReq(Resource):
+    def get(self):
+        raise_num = raise_count(files1)
+        return {"raiseNum" : raise_num}
+
 api.add_resource(Req, "/api/req")
+api.add_resource(RaiseReq,"/api/raise")
+
 
 @application.route('/', defaults={'path': ''})
 @application.route('/<path:path>')
