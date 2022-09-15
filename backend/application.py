c18ddf58fc755a90fbf66b493e3d0eb5876a22ee
kouphasi <phasi.ok.phasi@gmail.com>
2022-09-15 18:26:24 +0900

    復元&REST-APIの追加

--- backend/application.py
+++ backend/application.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
+from flask_restful import Api, Resource
+from count_only import request_number, files
 
 application = Flask(__name__, static_folder='./dist/static',
                     template_folder='./dist')
 
+api = Api(application)
+
+class Req(Resource):
+    def get(self):
+        num = request_number(files)
+        return { "num" :str(num)}
+
+api.add_resource(Req, "/api/req")
 
 @application.route('/', defaults={'path': ''})
 @application.route('/<path:path>')
@@ -11,4 +21,4 @@ def index(path):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
