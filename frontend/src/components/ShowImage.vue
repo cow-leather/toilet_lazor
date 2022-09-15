a0c31858def7c11e438eb2a96c7bdda19ceee909
KOU256 <25428510+KOU256@users.noreply.github.com>
2022-09-15 13:52:10 +0900

    ✨ 人数によって画像を表示する機能を実装
    現在の人数に応じて画像を切り替える機能を実装した．

---
+++ frontend/src/components/ShowImage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img :src="expressions[index]">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowImage',
+  props: {
+    n: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      index: 0,
+      expressions: [
+        require('@/assets/business_man_0.png'),
+        require('@/assets/business_man_1.png'),
+        require('@/assets/business_man_2.png'),
+        require('@/assets/business_man_3.png'),
+        require('@/assets/business_man_4.png')
+      ]
+    }
+  },
+  watch: {
+    name(n) {
+      if (n > 4) {
+        this.index = 4
+      } else {
+        this.index = n
+      }
+    }
+  }
+}
+</script>
--- frontend/src/views/ShowNum.vue
+++ frontend/src/views/ShowNum.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <h1 id="number" class="number">{{ n }}</h1>
+        <ShowImage :n="n"></ShowImage>
     </div>
 </template>
 
 <script>
+import ShowImage from '../components/ShowImage.vue'
+
 export default {
+    components: {
+        ShowImage
+    },
     data() {
         return {
             n: 0
